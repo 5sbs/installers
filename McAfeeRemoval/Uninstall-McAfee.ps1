d078837bd9db802752d5d5a4a11510db19d27adf
Jason Walker <jason.walker@5sbs.com.au>
2026-01-30 15:34:40 +0800

    Update Uninstall-McAfee.ps1

--- McAfeeRemoval/Uninstall-McAfee.ps1
+++ McAfeeRemoval/Uninstall-McAfee.ps1
@@ -10,6 +10,8 @@ $uninstallRegPath = 'HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Mc
 
 # Download MCPR Tool
 Write-Output '[INFO] Downloading McAfee Consumer Product Removal Tool (MCPR)'
+if (!(Test-Path $destination)) {
+    New-Item -ItemType Directory -Path $destination}
 Invoke-WebRequest -Uri $url -OutFile $zipFilePath -Method Get
 if (!(Test-Path $zipFilePath)) {
     Write-Output '[ERROR] Download failed! Exiting with error'
@@ -27,7 +29,7 @@ if (!(Test-Path $zipExtractedPath)) {
 
 # Run MCPR
 Write-Output '[INFO] Running MCPR Tool'
-$process = Start-Process "$zipExtractedPath\mccleanup.exe" -ArgumentList "-p StopServices,MFSY,PEF,MXD,CSP,Sustainability,MOCP,MFP,APPSTATS,Auth,EMproxy,FWdiver,HW,MAS,MAT,MBK,MCPR,McProxy,McSvcHost,VUL,MHN,MNA,MOBK,MPFP,MPFPCU,MPS,SHRED,MPSCU,MQC,MQCCU,MSAD,MSHR,MSK,MSKCU,MWL,NMC,RedirSvc,VS,REMEDIATION,MSC,YAP,TRUEKEY,LAM,PCB,Symlink,SafeConnect,MGS,WMIRemover,RESIDUEFWDRIVER,Redir,MSHR,WPS,MSSPlus -v -s" -PassThru
+$process = Start-Process "$zipExtractedPath\mccleanup.exe" -ArgumentList "-p StopServices,MFSY,PEF,MXD,CSP,Sustainability,MOCP,MFP,APPSTATS,Auth,EMproxy,FWdiver,HW,MAS,MAT,MBK,MCPR,McProxy,McSvcHost,VUL,MHN,MNA,MOBK,MPFP,MPFPCU,MPS,SHRED,MPSCU,MQC,MQCCU,MSAD,MSHR,MSK,MSKCU,MWL,NMC,RedirSvc,VS,REMEDIATION,MSC,YAP,TRUEKEY,LAM,PCB,Symlink,SafeConnect,MGS,WMIRemover,RESIDUEFWDRIVER,Redir,MSHR,WPS,MSSPlus -v -s" -PassThru -Wait
 $exitCode = $process | Select-Object -ExpandProperty ExitCode
 write-output "[INFO] MCPR finished running -- exit code $exitCode"
 
@@ -35,5 +37,8 @@ if (Test-Path $uninstallRegPath) {
     Write-Output '[INFO] Uninstall key found in Registry (tool did not remove). Manually removing'
     Remove-Item -Path $uninstallRegPath
 }
+Write-Output '[INFO] Tidying files'
+Remove-Item -Path $destination -Recurse
+
 Write-Output "[INFO] Finished! Exiting with code $exitCode"
 exit $exitCode
